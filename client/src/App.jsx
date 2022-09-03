dbe3c60c0f31fba737f999b421ecd578e4584332
jon-zawada <jonathan_zawada@yahoo.com>
2022-09-03 00:40:17 -0700

    working webpack and react app rendering

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>{'react setup from scratch without cra testing ts'}</div>
+  )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const DIST_DIR = path.join(__dirname, 'client', 'dist');
+const SRC_DIR = path.join(__dirname, 'client', 'src', 'index.jsx');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', //
+  entry: SRC_DIR,
+  devtool: 'inline-source-map',
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ]
+  },
+    resolve: {
+    extensions: ['.jsx', '.ts', '.js'],
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './client/dist/index.html'
+    })
+  ]
+}
