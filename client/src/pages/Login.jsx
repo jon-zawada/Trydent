74141c90696b89252b6e4a67ca719c12990f76c5
jon-zawada <jonathan_zawada@yahoo.com>
2022-09-10 02:26:35 -0700

    functional login component with state and styles

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const [state, setState] = React.useState({
+    email: '',
+    password: ''
+  });
+
+  const submit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setState((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  return (
+    <div className='full-screen-container'>
+      <div className='login-container'>
+        <h1 className='login-title'>Trydent</h1>
+        <form className='form'>
+          <div className='input-group'>
+            <label>Email</label>
+            <input
+              type='email'
+              name='email'
+              value={state.email}
+              onChange={(e) => onChangeHandler(e)}
+            />
+          </div>
+          <div className='input-group'>
+            <label>Password</label>
+            <input
+              type='password'
+              name='password'
+              value={state.password}
+              onChange={(e) => onChangeHandler(e)}
+            />
+          </div>
+          <button type='submit' className='login-button' onClick={submit}>Login</button>
+        </form>
+        <div className='register'>
+          <Link to='/register'>Register</Link>
+          <Link to='/email'>Forgot Password?</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
