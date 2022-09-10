8f20ef42292f07e1645bdc3228cd77c7111cb446
jon-zawada <jonathan_zawada@yahoo.com>
2022-09-10 02:38:28 -0700

    adds register page and routing

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+  const [state, setState] = React.useState({
+    email: '',
+    password: '',
+    confirm: ''
+  });
+
+  const submit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setState((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  return (
+    <div className='full-screen-container'>
+      <div className='login-container'>
+        <h1 className='login-title'>Trydent</h1>
+        <h1 className='login-sub-title'>Register for an account</h1>
+        <form className='form'>
+          <div className='input-group'>
+            <label>Email</label>
+            <input
+              type='email'
+              name='email'
+              value={state.email}
+              onChange={(e) => onChangeHandler(e)}
+            />
+          </div>
+          <div className='input-group'>
+            <label>Password</label>
+            <input
+              type='password'
+              name='password'
+              value={state.password}
+              onChange={(e) => onChangeHandler(e)}
+            />
+          </div>
+          <div className='input-group'>
+            <label>Confirm Password</label>
+            <input
+              type='password'
+              name='confirm'
+              value={state.confirm}
+              onChange={(e) => onChangeHandler(e)}
+            />
+          </div>
+          <button type='submit' className='login-button' onClick={submit}>Create Account</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
