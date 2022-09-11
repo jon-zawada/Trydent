2aeb052cd347a6c3363dda463d282a72507f579d
jon-zawada <jonathan_zawada@yahoo.com>
2022-09-11 00:56:03 -0700

    react router bug back and forward browser history resolved

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -3,23 +3,21 @@ import { Link, Outlet } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <>
+    <div>
       <div className='nav-wrapper'>
         <div className='nav'>
           <h1>Trydent</h1>
           <nav>
             <ul>
               <li className='nav-item'><Link to='/'>Home</Link></li>
-              <hr />
               <li className='nav-item'><Link to='/about'>About</Link></li>
-              <hr />
               <li className='nav-item'><Link to='/profile'>Profile</Link></li>
             </ul>
           </nav>
         </div>
       </div>
       <Outlet />
-    </>
+    </div>
   );
 };
 
