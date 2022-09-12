a11681a0abc8490e29164820a8aa472e42f7e3ed
jon-zawada <jonathan_zawada@yahoo.com>
2022-09-11 23:51:28 -0700

    implements post routes for user register and login

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
+const User = require('../db/index');
 
 const app = express();
 const PORT = 3000;
@@ -13,10 +15,55 @@ app.use('/', express.static(DIST_DIR));
 app.use(cors({
   origin: 'http://localhost:8080'
 }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 /* Routes */
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((response) => {
+      if (response === null) {
+        res.send('User email does not exist');
+      } else {
+        bcrypt.compare(req.body.password, response.password)
+          .then((isCorrectPassword) => {
+            if (isCorrectPassword) {
+              res.send('Login successful');
+            } else {
+              res.send('User email or password is incorrect');
+            }
+          });
+      }
+    })
+    .catch((err) => {
+      res.send(err); //implement better status code
+    });
+});
+
+app.get('/users', () => {
+
+});
+
+app.post('/users', (req, res) => {
+  User.find({ email: req.body.email })
+    .then((response) => {
+      if (response.length) {
+        res.send('A user with this email already exists');
+      } else {
+        bcrypt.hash(req.body.password, 10)
+          .then((hashedPassword) => {
+            const user = new User({ email: req.body.email, password: hashedPassword });
+            user.save()
+              .then(() => {
+                res.send('User successfully created');
+              });
+          });
+      }
+    });
+    //implement catches
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(DIST_DIR, 'index.html'));
 });
